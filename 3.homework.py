8e2ede2547aa6fc1d3e6bf410e08bff93bdfd958
luwei <davidlu1994@163.com>
2020-01-27 23:47:52 +0800

    3

--- 3.homework.py
+++ 3.homework.py
@@ -1,6 +1,6 @@
 #-*-coding:utf-8-*-
 # Author:Lu Wei
-import re
+import re,sys,os
 #1.匹配一篇英文文章的标题 类似 The Voice Of China
 # ret=re.findall('[a-zA-Z\s]+','The Voice Of China',flags=re.S)
 # print(ret)
@@ -31,59 +31,82 @@ import re
 #7.从类似9-25/3+7/399/42998+10568/14的表达式中匹配出乘法或除法
 # ret=re.findall('\d+(?:[/*]\d+)+','9-25/3+7/399/42998+10568/14')
 # print(ret)
-menu = {
-    '北京': {
-        '海淀': {
-            '五道口': {
-                'soho': {},
-                '网易': {},
-                'google': {}
-            },
-            '中关村': {
-                '爱奇艺': {},
-                '汽车之家': {},
-                'youku': {},
-            },
-            '上地': {
-                '百度': {},
-            },
-        },
-        '昌平': {
-            '沙河': {
-                '老男孩': {},
-                '北航': {},
-            },
-            '天通苑': {},
-            '回龙观': {},
-        },
-        '朝阳': {},
-        '东城': {},
-    },
-    '上海': {
-        '闵行': {
-            "人民广场": {
-                '炸鸡店': {}
-            }
-        },
-        '闸北': {
-            '火车战': {
-                '携程': {}
-            }
-        },
-        '浦东': {},
-    },
-    '山东': {},
-}
+# menu = {
+#     '北京': {
+#         '海淀': {
+#             '五道口': {
+#                 'soho': {},
+#                 '网易': {},
+#                 'google': {}
+#             },
+#             '中关村': {
+#                 '爱奇艺': {},
+#                 '汽车之家': {},
+#                 'youku': {},
+#             },
+#             '上地': {
+#                 '百度': {},
+#             },
+#         },
+#         '昌平': {
+#             '沙河': {
+#                 '老男孩': {},
+#                 '北航': {},
+#             },
+#             '天通苑': {},
+#             '回龙观': {},
+#         },
+#         '朝阳': {},
+#         '东城': {},
+#     },
+#     '上海': {
+#         '闵行': {
+#             "人民广场": {
+#                 '炸鸡店': {}
+#             }
+#         },
+#         '闸北': {
+#             '火车战': {
+#                 '携程': {}
+#             }
+#         },
+#         '浦东': {},
+#     },
+#     '山东': {},
+# }
+#
+# lst=[menu]
+# while lst:
+#     for key in lst[-1]:
+#         print(key)
+#     inp = input('inputsome:')
+#     if inp.upper()=='Q':
+#         break
+#     if inp.upper()=='B':
+#         lst.pop()
+#         continue
+#     dict = lst[-1].get(inp)
+#     lst.append(dict)
 
-lst=[menu]
-while lst:
-    for key in lst[-1]:
-        print(key)
-    inp = input('inputsome:')
-    if inp.upper()=='Q':
-        break
-    if inp.upper()=='B':
-        lst.pop()
-        continue
-    dict = lst[-1].get(inp)
-    lst.append(dict)
+path1=r'C:\Users\davidlu\PycharmProjects\luwei-Knightsplan'
+
+size=0
+l=[path1]
+while l:
+    path=l.pop()
+    lst=os.listdir(path)
+    for i in lst:
+        file_path=os.path.join(path,i)
+        if os.path.isdir(file_path):
+            l.append(file_path)
+            print(l)
+        elif os.path.isfile(file_path):
+            s=os.path.getsize(file_path)
+            size+=s
+print(size)
+
+
+
+
+
+
--- 4.计算机.py
+++ 4.计算机.py
@@ -8,5 +8,41 @@
 4)写函数，计算‘23’ ‘10/5’
 5)引用4)中写好的函数，完成'23/4'计算
 '''
-#1-2*((60-30+(-40/5)(9-25/3+7/399/42998+10568/14))-(-43)/(16-3*2))
+
+# '1+2- (3*  4-3/2+ (   3-2*(3+  5 -3*  -0.2-3.3*2.2 -8.5/ 2.4 )+10) +10)'
+import re
+# s=''.join(i for i in re.split('\s+',s))
+# print(re.search('\([^()]+\)',s))
+
+bracket=re.compile('\([^()]+\)')
+div_mul=re.compile(r'\d+(\.\d+)?[*/]-?\d+(\.\d+)?')
+# a='(3-2*(3+5-3*-0.2-3.3*2.2-8.5/2.4)'
+#
+# print(div_mul.search(a))
+def Div_mul(s):
+
+
+def calc():
+    while True:
+        # s = input('Please input the expression(q for quit):')  # 例：'1+2- (3*  4-3/2+ (   3-2*(3+  5 -3*  -0.2-3.3*2.2 -8.5/ 2.4 )+10) +10)'
+        s='1+2- (3*  4-3/2+ (   3-2*(3+  5 -3*  -0.2-3.3*2.2 -8.5/ 2.4 )+10) +10)'
+        if s == 'q':
+            break
+        else:
+            s = ''.join(i for i in re.split('\s+', s))
+        if not s.startswith('('):
+            s=('(%s)'%s)
+        while bracket.search(s):
+            s.replace('--','+')
+            s_search=bracket.search(s).group()
+            if div_mul.search(s_search):
+                s_search=s
+
+
+
+
+if __name__=='__main__':
+    calc()
+
+
 
